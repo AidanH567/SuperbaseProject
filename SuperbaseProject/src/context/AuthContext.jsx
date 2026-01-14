e583637bd54976a3ceca0e9ae841fe00c9ab3a8e
AidanH567 <herstikaidan@gmail.com>
2026-01-14 18:33:38 +0100

    adding signup page

--- SuperbaseProject/src/context/AuthContext.jsx
+++ SuperbaseProject/src/context/AuthContext.jsx
@@ -8,15 +8,12 @@ export const AuthContextProvider = ({ children }) => {
   const [session, setSession] = useState(undefined);
 
   useEffect(() => {
-  //1) Check on 1st render for a session (getSession())
   async function getInitialSession() {
     try {
       const { data, error } = await supabase.auth.getSession();
       if (error) {
         throw error;
       }
-
-      console.log(data.session);
       setSession(data.session);
     } catch (error) {
       console.error('Error getting session:', error.message);
@@ -24,15 +21,37 @@ export const AuthContextProvider = ({ children }) => {
   }
   getInitialSession()
 
-  //2) Listen for changes in auth state
+  supabase.auth.onAuthStateChange((_event, session) => {
+    setSession(session);
+    console.log('Session changed:', session);
+  })
 
   }, []);
 
-//Auth functions (signin, signup, logout)
+  //Auth functions (signin, signup, logout)
+  //Sign in (success, data, error)
+
+  const signInUser = async (email, password) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email.toLowerCase(),
+        password: password,
+      })
+      if (error) {
+        console.error('Supabase sign-in error:', error.message);
+        return { success: false, error: error.message };
+      }
+      console.log('Supabase sign-in success:', data);
+      return { success: true, data };
+    } catch (error) {
+      console.error('Unexpected error during sign-in:', error.message);
+      return { success: false, error: 'An unexpected error occurred. Please try again.' };
+    }
+  }
 
 
   return (
-    <AuthContext.Provider value={{ session }}>
+    <AuthContext.Provider value={{ session, signInUser }}>
       {children}
     </AuthContext.Provider>
   );
